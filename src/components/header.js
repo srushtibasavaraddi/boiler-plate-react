97ca6791a330b8f2a687bfe0d0605d8cc478a833
shwetaps605 <shwetaroy305@gmail.com>
2020-06-14 03:02:21 +0530

    Finalized the theme for landing page

--- src/components/header.js
+++ src/components/header.js
@@ -4,26 +4,24 @@ import {connect} from 'react-redux';
  const Header = (props)=>{
     return (
         <header>
-            <nav class="navbar navbar-expand-md navbar-light bg-light">
-                <div>
+            
+            <div class="main-header-container">
+                <img src="/images/logo.png" class="logo"/>
+                <p class="address-text">Sampige Nagar, Dharwad, Karnataka 580008</p>
+            </div>
+            <nav class="navbar navbar-expand-md">
+                <div class="nav-container">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active"><a class="nav-link"  data-toggle="collapse" data-target=".navbar-collapse.show"><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></a></li>
                         <li class="nav-item"><a class="nav-link"><NavLink to="/registration" activeClassName="is-active">Resgistration</NavLink></a></li>
                         <li class="nav-item"><a class="nav-link"><NavLink to="/gallery" activeClassName="is-active">Gallery</NavLink></a></li>
                         <li class="nav-item"><a class="nav-link"><NavLink to="/downloads" activeClassName="is-active">Downloads</NavLink></a></li>
                     </ul>
-                </div> 
-                <div>
                     <ul class="navbar-nav navbar-right">
-                        <li class="nav-item"><Link to="/login"><button class="btn btn-outline-success my-2 my-sm-0">Login</button></Link> </li>
+                        <li class="nav-item"><Link to="/login"><button class="btn btn-primary">Login</button></Link> </li>
                     </ul>
-                </div>
-            </nav>  
-
-            <div class="main-header-container">
-                <span><h1 class="header-text">Basavaraddi English Medium School</h1></span>
-                <span><img src="/images/logo.png" className="logo"/></span>
-            </div>
+                </div> 
+            </nav> 
 
             <h4>{props.announcement}</h4>
             

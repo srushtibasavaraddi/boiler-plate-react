d49f702034dd140fbd329f49291cb9916b20bb06
shwetaps605 <shwetaroy305@gmail.com>
2020-06-18 02:31:46 +0530

    Fixed footer

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,6 @@ import {connect} from 'react-redux';
         <header>
             <div class="wave-container">
                 <img src="/images/logo.png" class="logo"/>
-                <p class="address-text">Sampige Nagar, Dharwad, Karnataka 580008</p>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#EF4036" fill-opacity="0.9" d="M0,224L80,186.7C160,149,320,75,480,58.7C640,43,800,85,960,106.7C1120,128,1280,128,1360,128L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
             </div>
             <nav class="navbar navbar-expand-md">

4b28b646188bb15c97a27cf22f78c6f018f9d41f
shwetaps605 <shwetaroy305@gmail.com>
2020-06-19 03:57:55 +0530

    Finalized header and login page

--- src/components/header.js
+++ src/components/header.js
@@ -7,27 +7,28 @@ import {connect} from 'react-redux';
  const Header = (props)=>{
     return (
         <header>
-            <div class="wave-container">
-                <img src="/images/logo.png" class="logo"/>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#EF4036" fill-opacity="0.9" d="M0,224L80,186.7C160,149,320,75,480,58.7C640,43,800,85,960,106.7C1120,128,1280,128,1360,128L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+            <div class="header-container">
+                <div class="wave-container">
+                    <img src="/images/logo.png" class="logo"/> 
+                </div>
+                <div class="navigation-bar">
+                    <nav class="navbar navbar-expand-md">
+                        <div class="nav-container">
+                            <ul class="navbar-nav mr-auto">
+
+                                <li class="nav-item active"><a class="nav-link"><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></a></li>
+                                <li class="nav-item"><a class="nav-link"><NavLink to="/registration" activeClassName="is-active">Registration</NavLink></a></li>
+                                <li class="nav-item"><a class="nav-link"><NavLink to="/gallery" activeClassName="is-active">Gallery</NavLink></a></li>
+                                <li class="nav-item"><a class="nav-link"><NavLink to="/downloads" activeClassName="is-active">Downloads</NavLink></a></li>
+                            </ul>
+                            <ul class="navbar-nav navbar-right">
+                                <Link to="/login"><button class="btn btn-warning btn-lg">Login</button></Link> 
+                            </ul>
+                        </div> 
+                    </nav> 
+                </div>
+
             </div>
-            <nav class="navbar navbar-expand-md">
-                <div class="nav-container">
-                    <ul class="navbar-nav mr-auto">
-
-                        <li class="nav-item active"><a class="nav-link"><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></a></li>
-                        <li class="nav-item"><a class="nav-link"><NavLink to="/registration" activeClassName="is-active">Registration</NavLink></a></li>
-                        <li class="nav-item"><a class="nav-link"><NavLink to="/gallery" activeClassName="is-active">Gallery</NavLink></a></li>
-                        <li class="nav-item"><a class="nav-link"><NavLink to="/downloads" activeClassName="is-active">Downloads</NavLink></a></li>
-                    </ul>
-                    <ul class="navbar-nav navbar-right">
-                        <Link to="/login"><button class="btn btn-warning btn-lg">Login</button></Link> 
-                    </ul>
-                </div> 
-            </nav> 
-
-            
-            
         </header>
     )
 }

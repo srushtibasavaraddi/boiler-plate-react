6dd2242a06cceba60ba957cc30e4d114e06ddd78
shwetaps605 <shwetaroy305@gmail.com>
2020-06-19 17:56:37 +0530

    Finished downloads pdf

--- src/components/downloads.js
+++ src/components/downloads.js
@@ -7,14 +7,34 @@ import Dropdown from 'react-dropdown';
 
 const Downloads =(props)=>{
     return (
-        <div>
-            downloads
-            <Dropdown options={[1,2,3,4,5,6,7,8,9]} onChange={(e)=>{
-                props.dispatch(sortGrade(e.value));
-            }
-            }
-            value={props.grade.toString()} placeholder="Select Grade"/>
-            {props.downloads.map((item)=><DownloadComponent key={item.id} {...item}/>)}
+        <div class="jumbotron">
+
+            <div class="downloads-container">
+
+                <div class="heading">
+                    <h1>Study material availabale for download</h1>
+                </div>
+
+                <div class="sub-container">
+
+                    <p class="sub-heading">Select Grade:</p>
+
+                    <div class="dropdown-container">
+
+                        <Dropdown titleHelper="Classes" title="Select Grade" options={[1,2,3,4,5,6,7,8,9]} onChange={(e)=>{
+                            props.dispatch(sortGrade(e.value));
+                        }
+                        }
+                        value={props.grade.toString()}/>
+                        {props.downloads.map((item)=><DownloadComponent key={item.id} {...item}/>)}
+
+                    </div>
+
+                </div>
+
+            
+            </div>
+            
         </div>
     );
 }

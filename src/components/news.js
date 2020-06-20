81703c7284825a6da7199d2717225a5aa4d7db68
shwetaps605 <shwetaroy305@gmail.com>
2020-06-20 16:20:54 +0530

    Made news component responsive

--- src/components/news.js
+++ src/components/news.js
@@ -4,32 +4,40 @@ import NewsComponent from '../components/editNewsComponent';
 
 const News = (props)=>(
 
-    <div class="container">
+    <div class="jumbotron">
         
-        <div class="box1">
+        <div class="row">
 
-            <img src="/images/pic6.jpg" class="image"/>
-
-            <div class="text">
-                <h1 class="quote">"Education is our passport to the future. For tomorrow belongs to the people who prepare it today."</h1> <br/>
-                <p>A school is a medium of learning for children and is often regarded as a place of worship for the students. Writing an essay on my school is quite common among students. Here we have essays on My School of different lengths which would prove quite helpful to your children. You can choose the essay as per your length requirement and you shall find that essays have been written in quite easy to understand yet crisp language.</p>
-            </div>
-           
-        </div>
-
-        <div class="news-container">
-
-            <div class="title">
-                <h1 >Announcements</h1>
-            </div>
-
-            <div>
-                {props.news.map((item) => <div class="news"> 
-                    <NewsComponent key={item.id} {...item}/>
-        
+            <div class="col-xs-12 col-sm-6 col-md-8">
+                <div class="box-1">
+                    <img src="/images/pic6.jpg" class="image"/>
+                    <div class="text">
+                        <blockquote class="blockquote-reverse">
+                            <p>Education is our passport to the future. For tomorrow belongs to the people who prepare it today.</p>
+                            <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                        <p>A school is a medium of learning for children and is often regarded as a place of worship for the students. Writing an essay on my school is quite common among students. Here we have essays on My School of different lengths which would prove quite helpful to your children. You can choose the essay as per your length requirement and you shall find that essays have been written in quite easy to understand yet crisp language.</p>
+                    </div>
                 </div>
-                 )}
-            </div>   
+                
+            </div>
+
+            <div class="col-xs-6 col-sm-6 col-md-4">
+                <div class="news-container">
+                    <div class="title">
+                        <h1 >Announcements</h1>
+                    </div>
+                    <div>
+                        {props.news.map((item) => <div class="news"> 
+                            <NewsComponent key={item.id} {...item}/>
+                
+                        </div>
+                        )}
+                    </div> 
+                </div>
+                
+
+            </div>
 
         </div>
 
